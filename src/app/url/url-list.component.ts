47d229ff571003884a595b9badbb65cd5e75f236
Tamires <tamiressiqueira12@gmail.com>
2021-09-12 13:57:05 -0300

    ajustes do front em view de urls de usuario

--- src/app/url/url-list.component.ts
+++ src/app/url/url-list.component.ts
@@ -1,5 +1,5 @@
 import { Usuario } from '../model/usuario';
-import { Component, OnInit } from "@angular/core";
+import {  Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Url } from "../model/url";
 import { UsuarioServiceService } from '../service/usuario-service';
@@ -15,38 +15,53 @@ export class UrlListComponent implements OnInit{
   urlOriginal:string="";
 
  urlEncurtada:string="";
+ public _reload=false;
 
+ private reload() {
+     setTimeout(() => this._reload = true);
+     this.retornarUrls(this.usuario.id!);
+
+ }
   constructor(
     private usuarioService: UsuarioServiceService,
     private activateRoute:ActivatedRoute){
 
-  }
 
-  ngOnInit():void{
-    this.usuario.id=+this.activateRoute.snapshot.paramMap.get('id')!;
+
+    }
+    /**
+     */
+
+    ngOnInit():void{
+      this.usuario.id=+this.activateRoute.snapshot.paramMap.get('id')!;
     this.retornarUrls(this.usuario.id);
-  }
 
+  }
 retornarUrls(id:number):any{
+
+  setTimeout(() => this._reload = true);
   this.usuarioService.retrieveUrls(id).subscribe({
-    next:usuarios=>{
-      this._urlsUsuario=usuarios;
+    next:urls=>{
+      this._urlsUsuario=urls;
+
     },error:err=>console.log('Error',err)
   })
+
+
   console.log(this._urlsUsuario);
 }
+
+
+
 encurtarUrl():any{
     this.usuarioService.encurtarUrl( this.usuario,this.urlOriginal).subscribe({
     next:url=>{
       this.url=url;
     },error:err=>console.log('Error',err)
+
   })
+    this.reload();
 
-
-  console.log(this._urlsUsuario);
 }
-
-
-
 
 }
